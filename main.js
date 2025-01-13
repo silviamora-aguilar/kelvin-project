7cd2fb772f9aea25c95be051b2aaad99508d036e
silviamora-aguilar <silvia.mora.calpoly@gmail.com>
2025-01-12 21:23:24 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-const kelvin = 293
+// converting Silvia's age to dog years
+
+var myAge = 34; //defining my age
+
+var earlyYears = 2; //variable for first two years of a dog's life
+
+earlyYears *= 10.5; //muliplies early years by 10.5 and reassigns the value
+
+var laterYears = myAge - 2; //taking my age and subracting 2 for early years
+
+laterYears *=4; //multiplying and reassigning value to later years
+
+myAgeInDogYears = earlyYears + laterYears //Adding early and later years
+
+var myName = 'Silvia'.toLowerCase(); //name in all lower
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`); //Publishes human and dog years
+
+
